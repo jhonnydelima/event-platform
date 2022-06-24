bb120155576b7be3a9068d221c9e274325ae59cd
Jhonny Lima <jhonny.lima@funcionalcorp.com.br>
2022-06-24 02:44:50 -0300

    refactor: update Header and Event components

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,9 +1,7 @@
-import { Logo } from "../components/Logo";
+import { Header } from "../components/Header";
 
 export function Event() {
     return (
-        <header className="w-full py-5 flex items-center justify-center bg-gray-700 border-b border-gray-600">
-            <Logo />
-        </header>
+        <Header />
     )
 }
