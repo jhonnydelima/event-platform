056cb819b7e6d0f00a8e4129ab82dab29bdab336
Jhonny Lima <jhonny.lima@funcionalcorp.com.br>
2022-06-25 03:45:59 -0300

    refactor: update Lesson and Sidebar components to use data from graphcms

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,25 +1,48 @@
-import { CheckCircle } from 'phosphor-react';
+import { CheckCircle, Lock } from 'phosphor-react';
+import { format, isPast } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 
-export function Lesson() {
+interface LessonProps {
+	title: string;
+	slug: string;
+	availableAt: Date;
+	type: 'live' | 'class';
+	videoId: string;
+}
+
+export function Lesson({ title, slug, availableAt, type, videoId}: LessonProps) {
+	const isLessonAvailable = isPast(availableAt);
+	const availableAtFormatted = format(availableAt, "EEEE' • 'd' de 'MMMM' • 'k'h'mm", {
+		locale: ptBR,
+	});
+
 	return (
 		<a href="#">
       <span className="text-gray-300">
-				Domingo • 20 de junho • 19h00
+				{availableAtFormatted}
 			</span>
 
       <div className="rounded border border-gray-500 p-4 mt-2">
         <header className="flex items-center justify-between">
-        	<span className="text-sm text-blue-500 font-medium flex items-center gap-2">
-						<CheckCircle size={20} />
-						Conteúdo liberado
-					</span>
+        	{isLessonAvailable ? (
+						<span className="text-sm text-blue-500 font-medium flex items-center gap-2">
+							<CheckCircle size={20} />
+							Conteúdo liberado
+						</span>
+					) : (
+						<span className="text-sm text-orange-500 font-medium flex items-center gap-2">
+							<Lock size={20} />
+							Em breve
+						</span>
+					)}
+
           <span className="text-xs rounded py-[0.125rem] px-2 text-white border border-green-300 font-bold">
-						AO VIVO
+						{type === 'live' ? 'AO VIVO' : 'AULA PRÁTICA'}
 					</span>
         </header>
 
         <strong className="text-gray-200 mt-5 block">
-					Abertura do evento Ignite Lab
+					{title}
 				</strong>
       </div>
     </a>
